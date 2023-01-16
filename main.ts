d1f1cb108c75a3d45459ae312e0e3c416cda91dc
Rudy Escobar <67067784+rescobar9@users.noreply.github.com>
2023-01-15 19:57:08 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+radio.setGroup(1)
+basic.forever(function () {
+    radio.sendValue("x", input.acceleration(Dimension.X) / 10)
+    basic.pause(100)
+    radio.sendValue("y", input.acceleration(Dimension.Y) / 10)
+    basic.pause(100)
+})
